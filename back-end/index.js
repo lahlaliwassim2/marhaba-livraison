2e7009248645f1cf78651a90f4d913017646febd
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-01 11:52:17 +0100

    create an HTTP server using Node.js

---
+++ back-end/index.js
@@ -0,0 +1,16 @@
+/* *|MARKER_CURSOR|* */
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+
+
+
+
+
+
+/* Listening to the port and logging the message to the console. */
+app.listen(PORT, ()=>{
+    console.log(`server is running at PORT ${PORT}`)
+})
