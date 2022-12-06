40133972e30bd592efc8fbb21dc63b5f843bbaf8
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-06 14:18:33 +0100

    ajouter les routes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const { getAllUser, getOneUser, updateUser } = require('../controllers/userController')
-const { authMiddleware ,isAdmin} = require('../middlewares/AuthMiddleware')
+const { getOneUser, updateUser } = require('../controllers/userController')
+const { authMiddleware } = require('../middlewares/AuthMiddleware')
 const errorHandller =require('../middlewares/errorHandller')
 const tryCatch = require('../middlewares/tryCatch')
 
 const router = express.Router()
 
-router.get('/get-users',authMiddleware,isAdmin, tryCatch(getAllUser))
 router.get('/get-user/:id',authMiddleware, tryCatch(getOneUser))
 router.put('/update-user/:id',authMiddleware, tryCatch(updateUser))
 
