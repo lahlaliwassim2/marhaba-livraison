0fa4d1416be08b2a4e83fe4cfee10974f62104ec
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-06 14:16:39 +0100

    modification

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -13,16 +13,7 @@ const getOneUser = asyncHandler(async(req,res)=>{
         throw new Error(error)
     }
 })
-//Get All User
-const getAllUser = asyncHandler(async(req,res)=>{
-    try {
-        const getUsers = await User.find()
-        if(!getUsers)throw new Error("no user found")
-        else res.json({getUsers})
-    } catch (error) {
-        throw new Error("error")
-    }
-})
+
 //Update User 
 const updateUser = asyncHandler(async(req,res)=>{
     const {id} =req.user
@@ -46,6 +37,5 @@ const updateUser = asyncHandler(async(req,res)=>{
 })
 module.exports = {
     getOneUser,
-    getAllUser,
     updateUser
 }
