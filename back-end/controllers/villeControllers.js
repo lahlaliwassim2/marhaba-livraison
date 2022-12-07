bac2090b64a56ac77f21ff9949aa560135523114
soumia elyakni <selyakni@gmail.com>
2022-12-07 14:44:23 +0100

    add throw Error

--- back-end/controllers/villeControllers.js
+++ back-end/controllers/villeControllers.js
@@ -2,15 +2,17 @@ const Ville = require('../models/Ville')
 
 const addVille = async (req, res) => {
 
-    const name = req.body.name
-
+    const {name} = req.body
+    console.log(name)
     try{
-      const City = await  Ville.create({
-        name : name
-    })  
-    res.status(201).json('added to City Collection');
-    } catch {
-    res.status(400).json({ message: error.message });
+      const City = await Ville.create({
+        name,
+      }
+     ) 
+     if(City) res.status(201).json('added to City Collection');
+      else throw new Error("not created")
+    } catch (err) {
+    throw new Error(err)
     }
 
     
