4a7960fd1d3c7e187492806b81ea59c4198a004b
khadija-chennaoui <khadijach896@gmail.com>
2022-12-09 16:03:11 +0100

    generate password and send the login detals to livreur

--- back-end/middlewares/mailer.js
+++ back-end/middlewares/mailer.js
@@ -27,15 +27,14 @@ const main = (method,user)=> {
     }
 
     if(method=='Addlivreur'){
-        subject = 'Forget password'
+        subject = ' Tack your email and password '
         html = `<div style='height: 150px; width: 100%;'>
-        <h4>👋 Hi Livreur , doz t5dam ha 7na gadina lik masa2il</h4>
+        <h4> 👋 Hi Livreur ,doz t5dam ha 7na gadina lik masa2il</h4>
         <h5>Your email :${user.email}</h5>
         <h5>Your password :${Storage('stockPassword')}</h5>
-        ${Storage.clear()}
         <p>You are receiving this because you (or someone else) have requested the reset of the password for your account. <span style='font-weight: bold;'>MARHABA</span>,Please click on the following link, or paste this into your browser to complete the process:</p>
         <a href="http://localhost:${3000}/path_login_frontend">Login</a> 
-        
+        ${Storage.clear()}
       </div>`
     }
 
