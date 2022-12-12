a331103d3ba0c255bd7ec42404bfff3fb176b041
soumia elyakni <selyakni@gmail.com>
2022-12-12 16:34:31 +0100

    include footer in app component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,9 @@
+import Footer from "./components/client/footer/footer"
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <Footer/>
+    
   )
 }
