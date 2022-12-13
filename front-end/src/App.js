c2c39fe7312351327d0bfe388f5e8d8b883917ce
khadija-chennaoui <khadijach896@gmail.com>
2022-12-13 16:00:36 +0100

    rejister et login form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import CardProduct from "./components/client/cardProduct" 
+import Register from "./components/auth/register" 
+import Login from "./components/auth/login" 
 export default function App() {
   return (
    <>
    <Routes>
       <Route path="/card" element={<CardProduct/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
       <Route />
       <Route />
    </Routes>
