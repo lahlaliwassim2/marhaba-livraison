5e4b44681761a4131251a03d59ca2128671fd7ac
soumia elyakni <selyakni@gmail.com>
2022-12-18 16:43:32 +0100

    create orderdetails schéma

--- back-end/models/OrderDetails.js
+++ back-end/models/OrderDetails.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 
-const orderDetailsSchema = mongoose.Schema({
+const OrderDetailsSchema = mongoose.Schema({
     product_id:{
-       
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+    },
+    
+    quantity:{
+        type: Number,
+        default : 1 
+    }, 
+    price:{
+        type: Number,
     }
 })
+
+module.exports = mongoose.model('OrderDetails', OrderDetailsSchema)
