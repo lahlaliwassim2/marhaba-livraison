613e33cd27d1fd9e47befc83d714f1ed43125607
soumia elyakni <selyakni@gmail.com>
2022-12-20 09:36:10 +0100

    add listelivreur routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,15 +5,19 @@ import Store from './components/store/Store'
 import About from './components/about/About'
 import Navbar from "./components/navbar/Navbar";
 import ShoppingCarteProvider from './context/ShoppingCarteContext'
+// import listeLivreur from "./components/admin/livreursListe/listeLivreur"
+
 function App() {
   return (
    <ShoppingCarteProvider>
    <Navbar />
-   <Container className="mb-4 <">
+   <Container className="mb-4">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/about" element={<About />} />
+        {/* <Route path="/listelivreur" element={<listeLivreur />} />
+        <Route path="/addLivreurForm" element={<addLivreurForm />} /> */}
       </Routes>
    </Container>
    </ShoppingCarteProvider>
