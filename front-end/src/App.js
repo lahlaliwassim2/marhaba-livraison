da240b180981d91b4aff86aec2fbfffca239a70f
soumia elyakni <selyakni@gmail.com>
2022-12-21 23:37:56 +0100

    add admin tools routers "orders & livreurs" in App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,38 @@
-import { Container } from "react-bootstrap";
+// import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Store from "./components/store/Store";
 import About from "./components/about/About";
-import Navbar from "./components/navbar/Navbar"
+// import Navbar from "./components/navbar/Navbar"
 import ShoppingCarteProvider from "./context/ShoppingCarteContext";
 import Login from "./components/forms/login";
 import Register from "./components/forms/register";
+import Dashboard from "./components/admin/dashboard/dashboard";
+import ShowLivreur from "./components/admin/livreur/ShowLivreur";
+import ShowOrders from "./components/admin/orders/ShowOrders"
+// import Sidbar from "./components/admin/Sidbar/Sidbar";
+
+
+
 
 function App() {
   return (
     <ShoppingCarteProvider>
-      <Navbar />
-      <Container className="mb-4">
+      {/* <Navbar /> */}
+      <div className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+          
+            <Route path="/dashboard/livreurs" element={<ShowLivreur/>}/>
+            <Route path="/dashboard/orders" element={<ShowOrders/>}/>
+          </Route>
         </Routes>
-      </Container>
+      </div>
     </ShoppingCarteProvider>
   );
 }
