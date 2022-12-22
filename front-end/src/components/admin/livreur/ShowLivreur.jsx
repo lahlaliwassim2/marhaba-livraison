830d5748852bc52a3f8158308d1985acb0ba994b
soumia elyakni <selyakni@gmail.com>
2022-12-22 09:10:32 +0100

    remove half liste

--- front-end/src/components/admin/livreur/ShowLivreur.jsx
+++ front-end/src/components/admin/livreur/ShowLivreur.jsx
@@ -23,31 +23,10 @@ const ShowLivreur = () => {
        <input type={'tel'} className='' value="+212-631.982008" id="phone" disabled={(!update)?'disabled':''}  />
        </div>
    </div>
-   <div className='d-flex flex-row justify-content-evenly'>
-       <div className='col-4'>
-         <select name="ville" id="ville" className='w-100' disabled={(!update)?'disabled':''}>
-           <option value="1">Rabat</option>
-           <option value="2">youssoufia</option>
-         </select>
-       </div>
-       <div className='col-4'>
-       <select name="status" id="status" className='w-100' disabled={(!update)?'disabled':''}>
-           <option value="1">pending</option>
-           <option value="2">active</option>
-         </select>
-       </div>
-       <div className='d-flex flex-column col-2'>
-         <button className='w-75' onClick={()=>setUpdate(true)}>update</button>
-         <button className={(!update)?'d-none':'d-block w-75'} onClick={()=>setUpdate(false)}>save</button>
-       </div>
-   </div>
+   
    </Container>
 
-   
   
-     
-   
-     
    </>
   )
 }
