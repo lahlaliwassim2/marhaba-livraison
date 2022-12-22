ee085a8ab7420c1778b8dd3245adb3e07e019a5a
soumia elyakni <selyakni@gmail.com>
2022-12-22 01:33:30 +0100

    change link to navlink

--- front-end/src/components/admin/Sidbar/Sidbar.jsx
+++ front-end/src/components/admin/Sidbar/Sidbar.jsx
@@ -1,63 +1,63 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Logo from "../../../assets/logo/logo.png";
 import "./Sidbar.css"
 
 const Sidbar = () => {
   return (
     <div className="sidebar d-flex flex-column flex-shrink-0 p-3 text-bg-dark w-100 h-100" > 
-    <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto m-auto text-white text-decoration-none">
+    <NavLink to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto m-auto text-white text-decoration-none">
       {/* <svg className="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg> */}
       <span className="fs-4"><img id="logo" src={Logo} alt="..." /></span>
-    </Link>
+    </NavLink>
       <h5 className='text-center text-light'>Admin NAME</h5>
     <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
       <li>
-        <Link to="statistique" className="nav-link text-white">
+        <NavLink to="statistique" className="nav-link text-white">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg> */}
           Dashboard
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link to="orders" className="nav-link text-white">
+        <NavLink to="orders" className="nav-link text-white">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg> */}
           Orders
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link to="livreurs" className="nav-link text-white">
+        <NavLink to="livreurs" className="nav-link text-white">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg> */}
             Livreurs
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link to="clients" className="nav-link text-white">
+        <NavLink to="clients" className="nav-link text-white">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg> */}
           Clients
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link to="products" className="nav-link text-white">
+        <NavLink to="products" className="nav-link text-white">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg> */}
           Products
-        </Link>
+        </NavLink>
       </li>
     </ul>
     <hr/>
     <div>
     <ul className="nav nav-pills flex-column mb-auto">
       <li className="nav-item">
-        <Link to="categories" className="nav-link text-white" aria-current="page">
+        <NavLink to="categories" className="nav-link text-white" aria-current="page">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg> */}
           Categories
-        </Link>
+        </NavLink>
       </li>
       <li className="nav-item">
-        <Link to="villes" className="nav-link text-white" aria-current="page">
+        <NavLink to="villes" className="nav-link text-white" aria-current="page">
           {/* <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg> */}
           Villes
-        </Link>
+        </NavLink>
       </li>
     </ul>
     </div>
