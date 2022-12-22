5b7d525baee045e557542518d4cdfb6f1d692853
soumia elyakni <selyakni@gmail.com>
2022-12-23 00:48:43 +0100

    comment navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Store from "./components/store/Store";
 import About from "./components/about/About";
-import Navbar from "./components/navbar/Navbar"
+// import Navbar from "./components/navbar/Navbar"
 import ShoppingCarteProvider from "./context/ShoppingCarteContext";
 import Login from "./components/forms/login";
 import Register from "./components/forms/register";
@@ -19,7 +19,7 @@ import LivreurDash  from './components/livreur/dashboard.jsx'
 function App() {
   return (
     <ShoppingCarteProvider>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="mb-4">
         <Routes>
           
